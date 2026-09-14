440c8a2a83133a93e35f451caacfe91c36499cc9
abubuhammad <abubuhammad@users.noreply.github.com>
2026-09-14 20:59:04 +0100

    Remove legacy rollback backups and clean project

--- frontend.legacy/src/components/student/cards/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AvailableCourseCard } from './AvailableCourseCard';
-export { default as CompletedCourseCard } from './CompletedCourseCard';
-export { default as CertificateCard } from './CertificateCard';
